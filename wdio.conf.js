379722fa5a15993b780d5e27874352321cf84db8
Lesly-Nupert <lesly.nupert@gmail.com>
2025-11-06 01:44:17 +0100

    chore: waitforTimeout global to 60s for Jenkins

--- wdio.conf.js
+++ wdio.conf.js
@@ -112,7 +112,7 @@ exports.config = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 30000, // Jenkins
+    waitforTimeout: 60000, // Jenkins
     // waitforTimeout: 10000,
     //
     // Default timeout in milliseconds for request
