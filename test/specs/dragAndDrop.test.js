4b8abfb038282ea8d5b68550556a436d69d52cdb
Lesly-Nupert <lesly.nupert@gmail.com>
2025-11-06 12:22:29 +0100

    test: Grouping 10 tests in 1 for dragAndDrop module

--- test/specs/dragAndDrop.test.js
+++ test/specs/dragAndDrop.test.js
@@ -26,80 +26,34 @@ describe('Drag And Drop Tests', () => {
     // TESTS
     // ==================================
 
-    it('Should drag element to drop zone l2', async () => {
-        // dragAndDrop-L2
-        await browser.pause(1000)
+    it('Should drag and drop all elements, display success message, display retry text and button for return to Drag and Drop Page', async () => {
+        
         await DragAndDropPage.dragAndDropL2()
         await expect(DragAndDropPage.draggableL2).not.toBeDisplayed()
-        await browser.pause(1000)
-    });    
-
-    it('Should drag element to drop zone r3', async () => {
-        // dragAndDrop-R3
-        await browser.pause(1000)
+     
         await DragAndDropPage.dragAndDropR3()
         await expect(DragAndDropPage.draggableR3).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone r1', async () => {
-        // dragAndDrop-R1
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropR1()
         await expect(DragAndDropPage.draggableR1).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone c1', async () => {
-        // dragAndDrop-C1
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropC1()
         await expect(DragAndDropPage.draggableC1).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone c3', async () => {
-        // dragAndDrop-C3
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropC3()
         await expect(DragAndDropPage.draggableC3).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone r2', async () => {
-        // dragAndDrop-R2
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropR2()
         await expect(DragAndDropPage.draggableR2).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
 
-    it('Should drag element to drop zone c2', async () => {
-        // dragAndDrop-C2
-        await browser.pause(1000)
         await DragAndDropPage.dragAndDropC2()
         await expect(DragAndDropPage.draggableC2).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone l1', async () => {
-        // dragAndDrop-L1
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropL1()
         await expect(DragAndDropPage.draggableL1).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-    it('Should drag element to drop zone l3', async () => {
-        // dragAndDrop-L3
-        await browser.pause(1000)
+    
         await DragAndDropPage.dragAndDropL3()
         await expect(DragAndDropPage.draggableL3).not.toBeDisplayed()
-        await browser.pause(1000)
-    });
-
-
-    it('Should display success message, retry text and button  for return to Drag and Drop Page - This test depend of previous tests', async () => {
 
         const message = await DragAndDropPage.getSuccessMessage();
         expect(message).toBe('Congratulations');
@@ -111,6 +65,5 @@ describe('Drag And Drop Tests', () => {
 
         const checkTitle = await DragAndDropPage.getDragAndDropTitle();
         expect(checkTitle).toBe('Drag and Drop');
-        
     });
 });
